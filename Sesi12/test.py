d956d18db40bd7e5b2e2ad663e30d3cad9d533d9
Yosianus11 <yosianusantonio@gmail.com>
2021-12-25 21:20:34 +0700

    Sesi 4 - 12

--- Sesi12/test.py
+++ Sesi12/test.py
@@ -3,6 +3,7 @@ import unittest
 from my_sum import sum
 
 from fractions import Fraction
+
 class TestSum(unittest.TestCase):
     def test_list_int(self):
         """
@@ -16,7 +17,7 @@ class TestSum(unittest.TestCase):
         """
         Test that it can sum a list of fractions
         """
-        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(1, 4),Fraction(1, 4)]
         result = sum(data)
         self.assertEqual(result, 1)
 
